6030ee0ca5bc9f4f2d64fa3923d04c4419cc61c7
MininAS <mininas@sampo.ru>
2023-08-02 23:08:00 +0300

    Fixed main page manual

--- info/index.php
+++ info/index.php
@@ -8,10 +8,8 @@ switch($_GET["lang"]){
 			<tr><td><img src = 'img/k_home.png' alt='Home'/></td><td> - the key appears only when you are outside the home page to return to it.</td></tr>
 			<tr><td><img src = 'img/k_book.png' alt='Note book'/></td><td> - something like a small forum for discussions, comments and suggestions on the site.</td></tr>
 			<tr><td><img src = 'img/k_profile.png' alt='Your profile'/></td><td>
-				- your personal page, here you can read and send messages to other users,
-        change your data and monitor your progress. </td></tr>
-		    <tr><td><img src = 'img/letter.png' alt='Letter'/></td><td> - sometimes you can see an letter
-         that means you have one or more unread messages.<td></tr>
+				- your personal page, here you can read and send messages to other users, change your data and monitor your progress. </td></tr>
+		    <tr><td><img src = 'img/letter.png' alt='Letter'/></td><td> - sometimes you can see an letter that means you have one or more unread messages.<td></tr>
 			<tr><td><img src = 'img/k_stat.png' alt='Statistic'/></td><td> - statistics depending on where you are,
         if you are on your profile page - then the information will be on your achievements,
         if in the game, then according to the results of records and medals collected for the whole time.</td></tr>
@@ -23,24 +21,15 @@ switch($_GET["lang"]){
 			<tr><td><img src = 'img/k_lang_rus.png' alt='Lang'/></td><td> - language change - Russian / English.</td></tr>
 	    </table>
 		 <br>
-        The widget of social network VKontakte - if you liked this site, be sure to click on it
-        and don't forget to tell your friends! Of course, if you have a corresponding account.<br>
+        The widget of social network VKontakte - if you liked this site, be sure to click on it and don't forget to tell your friends! Of course, if you have a corresponding account.<br>
 		&nbsp&nbsp&nbsp&nbsp
-		A top list of players in the page left window there are awarded points for well-played matches.
-        The points are dynamic for both cases - grow in the process of visiting the site, and decreas - if the player visits less often.
-        More detailed statistics can be viewed by clicking on a list user item.<br>
+		A top list of players in the page left window there are awarded points for well-played matches. The points are dynamic for both cases - grow in the process of visiting the site, and decrease - if the player visits less often. More detailed statistics can be viewed by clicking on a list user item.<br>
 		&nbsp&nbsp&nbsp&nbsp
-        Right side - blocks of games with attached tournament tables of prize places according currently,
-        you can be here if you play better.
-        Therefore, the table with medals is not permanent and will always change if someone played more better or game was removed.
-        Records are already immortalized game results. You can be here also but it will be more difficult.<br>
+        Right side - blocks of games with attached tournament tables of prize places according currently, you can be here if you play better. Therefore, the table with medals is not permanent and will always change if someone played more better or game was removed. Records are already immortalized game results. You can be here also but it will be more difficult.<br>
 		&nbsp&nbsp&nbsp&nbsp
-		To select a game, you need to click on a game block by a picture or a  text, you will go to a zero game - this is a new layout of a field.
-        To compete, you can to choose a game from the tournament table or select it directly in the game
-        in the "Layout Combinations" list on the page right side, where an fully list of games will be presented.<br>
+		To select a game, you need to click on a game block by a picture or a  text, you will go to a zero game - this is a new layout of a field. To compete, you can to choose a game from the tournament table or select it directly in the game in the "Layout Combinations" list on the page right side, where an fully list of games will be presented.<br>
 		&nbsp&nbsp&nbsp&nbsp
-        What kind of competition is this - you choose someone else's game, and you are given the same layout of the field playing combination,
-        which a player who opened this game layout.<br><br>
+        What kind of competition is this - you choose someone else's game, and you are given the same layout of the field playing combination, which a player who opened this game layout.<br><br>
         &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 		Let a game be fair.
         <?php
@@ -53,14 +42,11 @@ switch($_GET["lang"]){
 		<table>
 			<tr><td><img src = 'img/k_home.png' alt='Home'/></td><td> - клавиша появляется только когда вы вне главной страницы для возврата к таковой.</td></tr>
 			<tr><td><img src = 'img/k_book.png' alt='Note book'/></td><td> - что - то вроде маленького форума для обсуждений, замечаний и просьб по сайту.</td></tr>
-			<tr><td><img src = 'img/k_profile.png' alt='Your profile'/><div class = 'letter'></div></td><td>
-			 - ваша личная страница, здесь можно читать и отправлять сообщения другим пользователям,
-		изменять свои данные и наблюдать за своими достижениями.</td></tr>
+			<tr><td><img src = 'img/k_profile.png' alt='Your profile'/></td><td>
+			 - ваша личная страница, здесь можно читать и отправлять сообщения другим пользователям, изменять свои данные и наблюдать за своими достижениями.</td></tr>
 		    <tr><td><img src = 'img/letter.png' alt='Letter'/></td><td> - вы можете иногда наблюдать конвертик,
 		 это означает, что у вас есть одно или несколько непрочитанных сообщений.</td></tr>
-			<tr><td><img src = 'img/k_stat.png' alt='Statistic'/></td><td> - статистика в зависимости от того где вы находитесь,
-		если вы у себя на странице - то информация будет по вашим наградам,
-		если в игре то по результатам рекордов и набранных медалей за все время.</td></tr>
+			<tr><td><img src = 'img/k_stat.png' alt='Statistic'/></td><td> - статистика в зависимости от того где вы находитесь, если вы у себя на странице - то информация будет по вашим наградам, если в игре то по результатам рекордов и набранных медалей за все время.</td></tr>
 			<tr><td><img src = 'img/k_pause.png' alt='Pause'/></td><td> - необходима в некоторых играх если вы вздумали отлучиться на не долго.</td></tr>
 			<tr><td><img src = 'img/k_new_game.png' alt='New game'/></td><td> - в режиме игры для начала новой комбинации игры.</td></tr>
 			<tr><td><img src = 'img/k_revert.png' alt='New game'/></td><td> - в режиме игры отменяет последний сделанный шаг.</td></tr>
@@ -71,21 +57,15 @@ switch($_GET["lang"]){
 		 <br>
 		Виджет "ВКонтакте" - если вам понравился этот сайт, обязательно нажмите и не забудьте рассказать друзьям! <br>
 		&nbsp&nbsp&nbsp&nbsp
-		Рейтинг сайта - список играков в левом окне страницы - баллы там начисляются за хорошо сыгранные партии.
-		Баллы данного счета динамичны как для роста в процессе посещения сайта, так и для уменьшения - если игрок заходит не так часто.
-		Более подробная статистика может быть просмотренна, если кликнуть по пользователю из этого списка.<br>
+		Рейтинг сайта - список игроков в левом окне страницы - баллы там начисляются за хорошо сыгранные партии. Баллы данного счета динамичны как для роста в процессе посещения сайта, так и для уменьшения - если игрок заходит не так часто. Более подробная статистика может быть просмотрена, если кликнуть по пользователю из этого списка.<br>
 		&nbsp&nbsp&nbsp&nbsp
-		Правая сторона - блоки игр с прикрепленными к ним турнирными таблицами призовых мест по текущему времени,
-		участников которых вы можете сдвинуть вниз, если у вас получится лучше.
-		Следовательно, таблица с медалями не носит постоянного характера и всегда, будет меняться.
-		Рекорды – это уже увековеченные партии в которых вы сможете сдвинуть соперника по баллам сыграв в любой из партии этой игры.
+		Правая сторона - блоки игр с прикрепленными к ним турнирными таблицами призовых мест по текущему времени, участников которых вы можете сдвинуть вниз, если у вас получится лучше. Следовательно, таблица с медалями не носит постоянного характера и всегда, будет меняться. Рекорды – это уже увековеченные партии в которых вы сможете сдвинуть соперника по баллам сыграв в любой из партии этой игры.
 		&nbsp&nbsp&nbsp&nbsp
-		Для выбора игры вам необходимо нажать по блоку игры по картинке или тексту, вы перейдете к нулевой игре - это новый расклад поля.
-		Для соревнования вы можите выбрать игру из турнирной таблицы или выбрать ее непосредственно в игре
-		в списке «Комбинации полей» в правой части экрана, где будет представлен весь список партий.<br>
+		Для выбора игры вам необходимо нажать по блоку игры по картинке или тексту, вы перейдете к нулевой игре - это новый расклад поля. Для соревнования вы можете выбрать игру из турнирной таблицы или выбрать ее непосредственно в игре в списке «Комбинации полей» в правой части экрана, где будет представлен весь список партий.<br>
 		&nbsp&nbsp&nbsp&nbsp
-		Что это за соревнования – вы выбираете чужую игру, и вам на поле предоставляется копия расклада игровой комбинации,
-		которая была у игрока, который ее открыл. А смысл в том, что игра будет честной, и совершенно не зависеть от разного расклада фишек.
+		Что это за соревнования – вы выбираете чужую игру, и вам на поле предоставляется копия расклада игровой комбинации, которая была у игрока, который ее открыл. А смысл в том, что игра будет честной, и совершенно не зависеть от разного расклада фишек.<br><br>
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+		Пусть игра будет честной.
 		<?php
 }
 ?>
